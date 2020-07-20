ec081e6f4d8377f1d08ceb8e059dda1e2649a3a4
unknown <qowhdgus629@naver.com>
2020-07-20 16:08:15 +0900

    navbar

--- app.py
+++ app.py
@@ -3,12 +3,23 @@ from flask import Flask , render_template
 app = Flask(__name__)
 app.debug=True
 
-@app.route('/data')
+@app.route('/')
 def index():
     print('seccess')
     # return 'TEST'
-    return render_template('home.html')
+    return render_template('home.html', hello='garykim')
 
+@app.route('/about')
+def about():
+    # print('seccess')
+    # return 'TEST'
+    return render_template('about.html', hello='garykim')
+
+@app.route('/Articles')
+def Articles():
+    print('seccess')
+    # return 'TEST'
+    return render_template('articles.html', hello='garykim')
 
 if __name__ == '__main__' :
     # app.run(host='0.0.0.0', port='8080')
