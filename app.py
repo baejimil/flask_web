ecaab50e8b3a4273c946a6f2ecc2e1616eaa0e43
unknown <qowhdgus629@naver.com>
2020-07-21 11:02:09 +0900

    sb-admin

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
-from flask import Flask , render_template
+from flask import Flask ,render_template
 from data import Articles
 app = Flask(__name__)
 app.debug=True
 
 @app.route('/')
 def index():
-    print('seccess')
-    # return 'TEST'
-    return render_template('home.html', hello='garykim')
+    print("Success")
+    # return "TEST"
+    return render_template('home.html',hello="GaryKim")
 
 @app.route('/about')
 def about():
-    # print('seccess')
-    # return 'TEST'
-    return render_template('about.html', hello='garykim')
+    print("Success")
+    # return "TEST"
+    return render_template('about.html',hello="GaryKim")
 
-@app.route('/articles')
-def Articles():
-    print('seccess')
-    # return 'TEST'
+@app.route('/articles', methods=['GET','POST'])
+def articles():
+    print("Success")
+    # return "TEST"
     articles = Articles()
-    print(len(articles))
-    return render_template('articles.html', articles=articles)
+    print(articles)
+    return render_template('articles.html',articles=articles)
 
-if __name__ == '__main__' :
+if __name__ =='__main__':
     # app.run(host='0.0.0.0', port='8080')
     app.run()
-    
