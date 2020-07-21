cea1d33b6db36b6928da43f76614cfb5f78e90b4
unknown <qowhdgus629@naver.com>
2020-07-21 17:36:18 +0900

    query

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , flash , redirect , url_for , session , request , logging
+from flask_mysqldb import MySQL
+import pymysql
 from data import Articles
 
 app = Flask(__name__)
 app.debug=True
+
+#config MySQL
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='1234'
+app.config['MYSQL_DB']='myflaskapp'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+
+db = pymysql.connect(host='localhost', 
+                        port=3306, 
+                        user='root', 
+                        passwd='1234', 
+                        db='myflaskapp')
+cursor = db.cursor()
+
+# init mysql
+# mysql = MySQL(app)
+
+# cur = mysql.connection.cursor()
+# result = cur.execute('SELECT * FROM users;')
+
+# users = cur.fetchall()
+# print(users)
+# print(result)
+
 @app.route('/')
 def index():
     print("Success")
--- mydb.py
+++ mydb.py
@@ -1,28 +1,80 @@
 import pymysql
-
 db = pymysql.connect(host='localhost', port=3306, user='root', passwd='1234', db='myflaskapp')
 cursor = db.cursor()
 # sql = ''' 
-#     CREATE TABLE users(
-#         id INT(11) AUTO_INCREMENT PRIMARY KEY, 
-#         name VARCHAR(100),
-#         email VARCHAR(100),
-#         username VARCHAR(30),
-#         password VARCHAR(100),
-#         register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
-#         ENGINE=InnoDB DEFAULT CHARSET=utf8;
+#         CREATE TABLE users(
+#             id INT(11) AUTO_INCREMENT PRIMARY KEY, 
+#             name VARCHAR(100),
+#             email VARCHAR(100),
+#             username VARCHAR(30),
+#             password VARCHAR(100),
+#             register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+#             ENGINE=InnoDB DEFAULT CHARSET=utf8;
 #     '''
-sql = ''' 
-    CREATE TABLE `topic` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`title` varchar(100) NOT NULL,
-	`body` text NOT NULL,
-	`author` varchar(30) NOT NULL,
-    `create_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (id)
-	) ENGINE=innoDB DEFAULT CHARSET=utf8;
-'''
+# sql=''' 
+#     CREATE TABLE `topic` (
+#     `id` int(11) NOT NULL AUTO_INCREMENT,
+#     `title` varchar(100) NOT NULL,
+#     `body` text NOT NULL,
+#     `author` varchar(30) NOT NULL,
+#     `create_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+#     PRIMARY KEY (id)
+#     ) ENGINE=innoDB DEFAULT CHARSET=utf8;
+# '''
+# cursor.execute(sql)
+# db.commit()
+# db.close()
 
-cursor.execute(sql)
+sql_1 = 'SELECT * FROM users;'
+sql_2=  '''
+        INSERT INTO users(name, email , username, password) 
+        VALUES ('PARK' ,'4@naver.com', 'PARK', '1234');
+            '''
+# cursor.execute(sql_2)
+# db.commit()
+# db.close()
+# print(result)
+# users = cursor.fetchall()
+# print(users[0][1])
+# cursor.execute(sql_1)
+# users = cursor.fetchall()
+# print(users)
+
+name = 'SONG'
+email = '5@naver.com'
+username = 'SONG'
+password = '1234'
+sql_3=  '''
+        INSERT INTO users(name, email , username, password) 
+        VALUES (%s , %s, %s, %s);
+            '''
+# cursor.execute(sql_3, (name, email , username, password))
+# db.commit()
+# db.close()
+
+name = 'gangnam'
+email = '6@naver.com'
+username = 'gangnam'
+password = '1234'
+sql_4=  '''
+        INSERT INTO users(name, email , username, password) 
+        VALUES (%s , %s, %s, %s);
+            '''
+# cursor.execute(sql_4, (name, email , username, password))
+# db.commit()
+# db.close()
+
+sql_5='DELETE FROM `myflaskapp`.`users` WHERE  `id`=11;'
+# cursor.execute(sql_5)
+# db.commit()
+# db.close()
+
+sql_6='DELETE FROM users WHERE name="SONG";'
+# cursor.execute(sql_6)
+# db.commit()
+# db.close()
+
+sql_7 = 'UPDATE `users` SET `name`="PARK" WHERE  `id`=6;'
+cursor.execute(sql_7)
 db.commit()
-db.close
+db.close()
