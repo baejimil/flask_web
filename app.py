79e003426e10043f28164778d4fbf4151059b626
unknown <qowhdgus629@naver.com>
2020-07-21 11:21:11 +0900

    SB-Admin

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
-from flask import Flask ,render_template
+from flask import Flask , render_template
 from data import Articles
+
 app = Flask(__name__)
 app.debug=True
-
 @app.route('/')
 def index():
     print("Success")
     # return "TEST"
-    return render_template('home.html',hello="GaryKim")
+    return render_template('home.html', hello = "GaryKim")
 
 @app.route('/about')
 def about():
     print("Success")
     # return "TEST"
-    return render_template('about.html',hello="GaryKim")
+    return render_template('about.html', hello = "GaryKim")
 
-@app.route('/articles', methods=['GET','POST'])
+@app.route('/articles')       #methods = ['GET','POST'])로 추가 가능
 def articles():
     print("Success")
     # return "TEST"
     articles = Articles()
     print(articles)
-    return render_template('articles.html',articles=articles)
+    return render_template('articles.html', articles = articles)
+
+@app.route('/test')
+def show_image():
+    return render_template('image.html')
 
 if __name__ =='__main__':
-    # app.run(host='0.0.0.0', port='8080')
+    # app.run(host = '0.0.0.0', port='8080')
     app.run()
