51d5786f111abb666de9213c24fe03a58ca9213f
unknown <qowhdgus629@naver.com>
2020-07-24 14:17:28 +0900

    login check

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask , render_template, flash, redirect, url_for, session, request, logging
+from flask import Flask ,render_template , flash , redirect , url_for, session, request, logging
 
 import pymysql
-from data import Articles
 from passlib.hash import pbkdf2_sha256
-
-
+from data import Articles
+from functools import wraps
 app = Flask(__name__)
 app.debug=True
+
 
 db = pymysql.connect(host='localhost', 
                         port=3306, 
@@ -15,50 +15,31 @@ db = pymysql.connect(host='localhost',
                         db='myflaskapp')
 
 
-# sql_1 = 'SELECT * FROM users;'
-# # sql_2 = '''
-# #         INSERT INTO users(name, email, userame, password) 
-# #         VALUES ('LEE , '3@naver.com','LEE','1234');
-# #         '''
-# # result = cursor.execute(sql_2)
-# # db.commit()
-# # db.close()
-# # # print(result)
-# # users = cursor.fetchall()
-# # print(users[0][1])
-# cursor.execute(sql_1)
-# users = cursor.fetchall()
-# print(users)
-#init mysql
+#init mysql 
 # mysql = MySQL(app)
+# cur  = mysql.connection.cursor()
+# result  = cur.execute("SELECT * FROM users;")
 
-# cur = mysql.connection,cursor()
-# result = cur.execute("SELECT * FROM isers;")
-# users - 
-
-
+# users  = cur.fetchall()
+# print(users)
 # print(result)
 
+def is_LOGED_out(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'is_LOGED' in session:
+        # if session['is_LOGED'] :
+            return redirect(url_for('articles'))
+        else:
+            return f(*args, **kwargs)
 
+    return wrap
 
-
-@app.route('/')
-def index():
-    print("Success")
-    # return "TEST"
-    return render_template('home.html', hello = "GaryKim")
-
-@app.route('/about')
-def about():
-    print("Success")
-    # return "TEST"
-    return render_template('about.html', hello = "GaryKim")
-
-
-######################## 회원가입 ###############################
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register',methods=['GET' ,'POST'])
+@is_LOGED_out
 def register():
     if request.method == 'POST':
+
         # data = request.body.get('author')
         name = request.form.get('name')
         email = request.form.get('email')
@@ -66,93 +47,187 @@ def register():
         re_password = request.form.get('re_password')
         username = request.form.get('username')
         # name = form.name.data
-        if(pbkdf2_sha256.verify(re_password,password)):
-            print(pbkdf2_sha256.verify(re_password,password))
+        if(pbkdf2_sha256.verify(re_password,password )):
+            print(pbkdf2_sha256.verify(re_password,password ))
             cursor = db.cursor()
             sql = '''
                 INSERT INTO users (name , email , username , password) 
-                VALUES (%s ,%s,%s,%s )
+                VALUES (%s ,%s, %s, %s )
              '''
             cursor.execute(sql , (name,email,username,password ))
             db.commit()
             
+
             # cursor = db.cursor()
             # cursor.execute('SELECT * FROM users;')
             # users = cursor.fetchall()
-            return "register Success"
+            
+            return redirect(url_for('login'))
+
         else:
             return "Invalid Password"
+
         db.close()
     else:
         return render_template('register.html')
 
 
-# # 회원가입
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if request.method == 'POST':
-
-#         # data = request.body.get('author')
-#         name = request.form.get('name')
-#         email = request.form.get('email')
-#         username = request.form.get('username')
-#         password = request.form.get('password')
-#         re_password = request.form.get('re_password')
-#         if(password is not re_password):
-#             return 'Invalid password'
-#         else:
-#         # name = form.name.data
-#             print([name, email, username, password,re_password])
-#             return "POST success"
-#     else:
-#         return "GET success"
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login',methods=['GET', 'POST'])
+@is_LOGED_out
 def login():
     if request.method == 'POST':
-        id = request.form.get('email')
+        id = request.form['email']
         pw = request.form.get('password')
-        print([id, pw])
+        print([id])
 
         sql='SELECT * FROM users WHERE email = %s'
-        cursor = db.cursor()
+        cursor  = db.cursor()
         cursor.execute(sql, [id])
         users = cursor.fetchone()
         print(users)
 
-        if users == None:
+        if users ==None:
             return redirect(url_for('login'))
-
         else:
-            if pbkdf2_sha256.verify(pw,users[4]):
+            if pbkdf2_sha256.verify(pw,users[4] ):
+                session['is_LOGED'] = True
+                session['username'] = users[3]
+                print(session)
                 return redirect(url_for('articles'))
             else:
                 return redirect(url_for('login'))
-        # return "success"
+        
     else:
         return render_template('login.html')
+
+
+
+def is_LOGED_in(f):
+    @wraps(f)
+    def _wraper(*args, **kwargs):
+        if 'is_LOGED' in session:
+        # if session['is_LOGED'] :
+            return f(*args, **kwargs)
+
+        else:
+            flash('UnAuthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return _wraper
+
+@app.route('/logout')
+@is_LOGED_in
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
     
 
+@app.route('/')
+@is_LOGED_in
+def index():
+    print("Success")
+    # session['test']= "Gary Kim"
+    # session_data = session
+    # print(session_data)
+    # return "TEST"
+    return render_template('home.html')
 
-@app.route('/articles')       #methods = ['GET','POST'])로 추가 가능
-def articles():
+@app.route('/about')
+@is_LOGED_in
+def about():
     print("Success")
     # return "TEST"
-    articles = Articles()
-    print(articles)
-    return render_template('articles.html', articles = articles)
+    return render_template('about.html')
 
-@app.route('/test')
-def show_image():
-    return render_template('image.html')
+@app.route('/articles')
+@is_LOGED_in
+def articles():
+    # data = Articles()
+    # print(len(articles))
+    cursor = db.cursor()
+    sql='SELECT * FROM topic;'
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    print(data)
+    return render_template('articles.html',articles=data)
+    # return "GET Success"
 
-@app.route('/article/<int:id>')
+
+@app.route('/article/<string:id>')
+@is_LOGED_in
 def article(id):
-    print(id)
-    articles = Articles()[id-1]
-    print(articles)
-    return render_template('article.html',data = articles) 
-    # return "Success"
+    # print(type(id))
+    # articles= Articles()[id-1]
+    cursor = db.cursor()
+    sql = 'SELECT * FROM topic WHERE id= %s;'
+    cursor.execute(sql, [id])
+    topic = cursor.fetchone()
+    print(topic)
+    return render_template('article.html',data =topic)
+    
+
+@app.route('/add_articles',methods=['GET','POST'])
+@is_LOGED_in
+def add_articles():
+    if request.method == 'POST':
+        # print(request.form['title'])
+        title = request.form['title']
+        body = request.form['body']
+        author = request.form['author']
+        cursor = db.cursor()
+        sql = '''
+            INSERT INTO topic (title, body , author)
+            VALUES (%s ,%s ,%s)
+        '''
+        cursor.execute(sql,(title, body , author))
+        db.commit()
+
+        # sql = 'SELECT * FROM topic WHERE id= %s;'
+        # cursor.execute(sql, [id])
+        # topic = cursor.fetchone()
+        return redirect("/articles")
+    else:
+        return render_template('add_articles.html')
+
+   
+
+@app.route('/article/<string:id>/edit_article',methods=['GET', 'POST'])
+@is_LOGED_in
+def edit_article(id):
+    if request.method =="POST":
+        title = request.form['title']
+        body = request.form['body']
+        author = request.form['author']
+        cur = db.cursor()
+        sql = '''
+            UPDATE `topic` SET `title`=%s,`body`=%s, `author`=%s  WHERE  `id`= %s;
+        '''
+        cur.execute(sql , (title,body,author, id ))
+        db.commit()
+        return redirect(url_for('articles'))
+    else:
+        print(id)
+        cur = db.cursor()
+        sql = 'SELECT * FROM topic WHERE id=%s'
+        cur.execute(sql , [id])
+        topic = cur.fetchone()
+        return render_template('edit_article.html', data= topic)
+  
+
+@app.route('/delete/<string:id>', methods=['POST'])
+@is_LOGED_in
+def delete(id):
+    cursor = db.cursor()
+    sql = 'DELETE FROM `topic` WHERE  `id`=%s'
+    cursor.execute(sql ,[id])
+    db.commit()
+    
+    return redirect(url_for('articles'))
+
 if __name__ =='__main__':
-    # app.run(host = '0.0.0.0', port='8080')
+    # app.run(host='0.0.0.0', port='8080')
+    
+    # Session 실행시 설정
+    # 오류: Set the secret_key on the application to something unique and secret.
+    app.secret_key = 'secretKey1234'
+    # 서버실행
     app.run()
